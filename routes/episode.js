cb46a28051884060bdea8e1c79d1b216ac46308b
minhvip2001 <minhnb.nde19025@vtc.edu.vn>
2023-07-14 18:14:07 +0700

    Create api update episode

--- routes/episode.js
+++ routes/episode.js
@@ -12,6 +12,10 @@ const ytdl = require('ytdl-core');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const filmPath = path.join('uploads', 'films');
+const { promisify } = require('util');
+
+
+const unlinkAsync = promisify(fs.unlink);
 
 // @route Get Episode
 // @desc Get Episode
@@ -124,6 +128,136 @@ Router.post("/", addFullUrl, async (req, res) => {
         episode.poster = `${req.fullUrl}/${episode.poster}`;
         episode.video = `${req.fullUrl}/${episode.video}`;
         res.status(201).json(episode);
+    } catch (err) {
+        console.error(err);
+        await session.abortTransaction();
+        res.status(500).json({ error: 'Internal server error' });
+    } finally {
+        session.endSession();
+    }
+});
+
+// @route Patch Episode
+// @desc Patch A Episode
+// @access Public
+Router.patch("/:id", addFullUrl, async (req, res) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+        const updates = Object.keys(req.body);
+        const allowedUpdates = [
+            "title",
+            "description",
+            "episode",
+            "video",
+        ];
+        const invalidUpdates = updates.filter((update) =>
+            !allowedUpdates.includes(update)
+        );
+
+        if (invalidUpdates.length > 0) {
+            return res.status(400).json({ error: "Invalid updates", invalidFields: invalidUpdates, allowUpdateFields: allowedUpdates });
+        }
+
+        const id = req.params.id
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            console.error(`Invalid episode ID: ${id}`);
+            return res.status(400).json({ error: `Invalid episode ID: ${id}` });
+        }
+
+        const episode = await Episode.findById(id).populate('film');
+        if (!episode) {
+            return res.status(404).json({ message: 'Episode not found' });
+        }
+
+        const film = await Film.findById(episode.film._id);
+        if (!film) {
+            return res.status(404).json({ message: 'Film not found' });
+        }
+
+        // Check title film exists
+        const existingEpisode = await Episode.findOne({ film: film._id, episode: req.body.episode });
+        if (id != existingEpisode._id) {
+            if (existingEpisode) {
+                return res.status(400).json({ error: `There is already episode '${existingEpisode.episode}' in this film '${film.title}'` });
+            }
+        }
+
+        // Remove the old poster image file
+        if (episode.poster) {
+            if (fs.existsSync(path.join(episode.poster))) {
+                await unlinkAsync(path.join(episode.poster));
+            }
+        }
+
+        if (episode.video) {
+            if (fs.existsSync(path.join(episode.video))) {
+                await unlinkAsync(path.join(episode.video));
+            }
+        }
+
+        // Dowload video youtube and save it to folder
+        const episodeVideoInfo = await ytdl.getInfo(req.body.video);
+        const episodeVideoName = `video-${uuidv4()}.mp4`;
+        const episodeVideoPath = path.join(filmPath, episodeVideoName);
+        const downloadVideoPromise = ytdl(req.body.video, {
+            format: 'mp4',
+            quality: 'highest',
+            filter: "audioandvideo",
+        }).pipe(fs.createWriteStream(episodeVideoPath));
+
+        const episodeThumbnailUrl = episodeVideoInfo.videoDetails.thumbnails.slice(-1)[0].url;
+        const episodeThumbnailName = `thumbnail-${uuidv4()}.png`;
+        const episodeThumbnailPath = path.join(filmPath, episodeThumbnailName);
+        const downloadThumbnailPromise = axios.get(episodeThumbnailUrl, { responseType: 'arraybuffer' })
+            .then(response => {
+                const imageBuffer = Buffer.from(response.data);
+
+                return sharp(imageBuffer)
+                    .png()
+                    .toBuffer();
+            })
+            .then(async pngBuffer => {
+                return await fs.promises.writeFile(episodeThumbnailPath, pngBuffer);
+            });
+
+        const slug = slugify(`${episode.film.title} ${req.body.title} ${req.body.episode}`, {
+            lower: true,
+            strict: true,
+        });
+        req.body.slug = slug;
+        req.body.poster = episodeThumbnailPath;
+        req.body.video = episodeVideoPath;
+
+        // Update the film object with the request body
+        Object.assign(episode, req.body);
+        const savePromise = episode.save()
+        await Promise.all([downloadVideoPromise, downloadThumbnailPromise, savePromise]);
+        await session.commitTransaction();
+
+        const obj = {
+            "_id": episode._id,
+            "title": episode.title,
+            "description": episode.description,
+            "episode": episode.episode,
+            "video": `${req.fullUrl}/${episode.video}`,
+            "poster": `${req.fullUrl}/${episode.poster}`,
+            "slug": episode.slug,
+            "date": episode.date,
+            "film": {
+                "_id": episode.film._id,
+                "title": episode.film.title,
+                "titleSearch": episode.film.titleSearch,
+                "poster": `${req.fullUrl}/${episode.film.poster}`,
+                "description": episode.film.description,
+                "actor": episode.film.actor,
+                "genre": episode.film.genre,
+                "reviews": episode.film.reviews,
+                "slug": episode.film.slug,
+                "date": episode.film.date,
+            },
+        }
+        res.json(obj);
     } catch (err) {
         console.error(err);
         await session.abortTransaction();
